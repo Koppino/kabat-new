1ed5044bea999aa9275001077eddf389357a69fa
Koppino <60857913+Koppino@users.noreply.github.com>
2025-04-29 10:31:00 +0200

    fb yt buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,18 +5,33 @@ import { useAuthContext } from "./context/AuthContext";
 import Login from "./pages/Login";
 import { Toaster } from "react-hot-toast";
 import Dashboard from "./pages/Dashboard";
+import "./App.css";
+import { FaFacebookF } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa";
 function App() {
   const { authUser } = useAuthContext();
   return (
     <>
       <div className="w-full" id="uvod">
         <Navbar />
-        <div className="">
+        <div className="relative flex items-center justify-center">
           <img
             alt="bg"
             src="header_img-72b6ce16.jpg"
             className="bg-cover bg-center  w-full"
           />
+          <div className="absolute bottom-[25%]">
+            <a href="https://www.facebook.com/KabatTribute/" target="_blank">
+              <button className="fb-btn">
+                <FaFacebookF className="icon inline" /> FACEBOOK
+              </button>
+            </a>
+            <a href="https://www.youtube.com/@KabatTribute" target="_blank">
+              <button className="yt-btn">
+                <FaYoutube className="icon inline" /> YOUTUBE
+              </button>
+            </a>
+          </div>
         </div>
         <Routes>
           <Route path="/" element={<Home />}></Route>
